31fd0f8d98aea6aa41b5a76950e6006798919f23
Violeta <draicavioleta26@gmail.com>
2025-05-30 09:34:55 +0300

    Update: modificat build.gradle.kts (ex: adăugat dependency)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,21 +62,13 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    // ✅ Retrofit + Gson
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
-    // ✅ Room (DB) + kapt
     implementation("androidx.room:room-runtime:2.6.1")
     kapt("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-
-    // ✅ Coil (for images)
     implementation("io.coil-kt:coil-compose:2.4.0")
-
-    // ✅ ViewModel Compose
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-
-    // ✅ Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.7.3")
 }
